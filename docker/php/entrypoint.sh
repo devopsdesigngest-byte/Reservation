3a4a75679026b118ae99155e7463771295f4ad9f
Ndiasse Mbow <NdiasseMbow@gmail.com>
2026-09-12 08:51:45 +0000

    fix: utiliser DB_HOST/DB_PORT au lieu du hostname fixe 'db' pour compatibilité Railway

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 set -e
+
+DB_HOST="${DB_HOST:-db}"
+DB_PORT="${DB_PORT:-3306}"
+
 until php -r '
-$socket = @fsockopen("db", 3306);
+$socket = @fsockopen($argv[1], (int)$argv[2]);
 if ($socket) {
     fclose($socket);
     exit(0);
 }
 exit(1);
-'; do
+' "$DB_HOST" "$DB_PORT"; do
     sleep 2
 done
+
 php bin/console ndiassembow:migrate
 exec php -S 0.0.0.0:8000 -t public
